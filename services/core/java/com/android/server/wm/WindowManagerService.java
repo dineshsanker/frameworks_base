f357d34a6b191fdf02417cef154fbaffa9049548
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-12-07 13:51:00 +0200

    Don't report existing menu key if it was remapped. [2/3]
    Change-Id: Iabad06cd42cebecdff6f0440899432513681b063
    Signed-off-by: dlasdl <dineshdlas@live.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5942,6 +5942,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean hasPermanentMenuKey() {
+        return mPolicy.hasPermanentMenuKey();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
